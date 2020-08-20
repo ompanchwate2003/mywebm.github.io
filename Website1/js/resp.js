b80ec90849827167748a5a068fe65862bf29eb7d
ompanchwate2003 <69083055+ompanchwate2003@users.noreply.github.com>
2020-08-20 23:00:44 +0530

    my first site

---
+++ Website1/js/resp.js
@@ -0,0 +1,10 @@
+ burger= document.querySelector(".burger");
+ navbar= document.querySelector(".nav-bar");
+ navlist= document.querySelector(".nav-list");
+ rightnav= document.querySelector(".right-nav");
+
+ burger.addEventListener('click', ()=>{
+     rightnav.classList.toggle("v-class-resp")
+     navlist.classList.toggle("v-class-resp")
+     navbar.classList.toggle("h-class-resp")
+ })
